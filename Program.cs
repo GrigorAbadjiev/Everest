a68830a1e57d743c9bcad2c149451786a23a9de8
User <User@DESKTOP-ESP2JT1>
2022-12-15 17:07:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+
+namespace _05._Everest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            double startingHeight = 5364;
+            double target = 8848;
+            double currentHeight = 0;
+            int days = 1;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+                if (command == "Yes")
+                {
+                    days++;
+                }
+                if (days > 5) break;
+
+                double height = double.Parse(Console.ReadLine());
+                currentHeight += height;
+
+                if (currentHeight + startingHeight >= target)
+                {
+                    break;
+                }
+            }
+
+            if (startingHeight + currentHeight < target)
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine($"{startingHeight + currentHeight}");
+            }
+            else
+            {
+                Console.WriteLine($"Goal reached for {days} days!");
+            }
+        }
+    }
+}
